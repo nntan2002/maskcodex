381a1cec41d45ed6cff5c9116beb768f0481e868
Thien._.An <nntan2002@gmail.com>
2024-04-02 22:54:01 +0700

    add components

---
+++ src/components/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer(){
+    return (
+        <footer>
+            đây là footer
+        </footer>
+    )
+}
---
+++ src/components/header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header>
+            Đây là header
+        </header>
+    )
+}
